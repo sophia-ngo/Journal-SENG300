a3b65f2f515d01b1bdc0c7ac4b6e95d6079682a7
Sophia <45104840+sophia-ngo@users.noreply.github.com>
2020-03-05 11:46:46 -0700

    changed fonts

---
+++ Login.java
@@ -0,0 +1,97 @@
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class Login extends JPanel {
+	private JTextField usernameTEXT;
+	private JTextField passwordTEXT;
+
+	/**
+	 * Create the panel.
+	 */
+	public Login(JFrame frame, Authenticator auth) {
+		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Welcome to U of C");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("LM Mono 10", Font.BOLD, 16));
+		lblNewLabel.setBounds(105, 24, 239, 16);
+		add(lblNewLabel);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setFont(new Font("LM Mono 10", Font.BOLD, 16));
+		lblUsername.setBounds(99, 82, 87, 16);
+		add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("LM Mono 10", Font.BOLD, 16));
+		lblPassword.setBounds(99, 137, 73, 16);
+		add(lblPassword);
+		
+		usernameTEXT = new JTextField();
+		usernameTEXT.setFont(new Font("LM Mono 10", Font.PLAIN, 12));
+		usernameTEXT.setBounds(206, 77, 130, 26);
+		add(usernameTEXT);
+		usernameTEXT.setColumns(10);
+		
+		passwordTEXT = new JTextField();
+		passwordTEXT.setFont(new Font("LM Mono 10", Font.PLAIN, 12));
+		passwordTEXT.setBounds(206, 132, 130, 26);
+		add(passwordTEXT);
+		passwordTEXT.setColumns(10);
+		
+		JLabel lblInvalidLogin = new JLabel("Invalid Login");
+		lblInvalidLogin.setHorizontalAlignment(SwingConstants.CENTER);
+		lblInvalidLogin.setFont(new Font("LM Mono 10", Font.BOLD, 16));
+		lblInvalidLogin.setBounds(142, 235, 165, 16);
+		add(lblInvalidLogin);
+		lblInvalidLogin.setVisible(false);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setFont(new Font("LM Mono 10", Font.BOLD, 16));
+		btnLogin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String user = usernameTEXT.getText();
+				String pass = passwordTEXT.getText();
+				Account acc = auth.login(user, pass);
+				if (acc == null) {
+					lblInvalidLogin.setVisible(true);
+				}else {
+					MainScreen panel = new MainScreen(frame, acc);
+					frame.setContentPane(panel);
+					frame.revalidate();
+				}
+			}
+		});
+		
+		
+
+		btnLogin.setBounds(99, 194, 117, 29);
+		add(btnLogin);
+		
+		JButton btnRegister = new JButton("Register");
+		btnRegister.setFont(new Font("LM Mono 10", Font.BOLD, 16));
+		btnRegister.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Register panel = new Register(frame, auth);
+				frame.setContentPane(panel);
+				frame.revalidate();
+			}
+		});
+		btnRegister.setBounds(228, 194, 117, 29);
+		add(btnRegister);
+		
+
+
+	}
+}
---
+++ MainScreen.java
@@ -0,0 +1,33 @@
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class MainScreen extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public MainScreen(JFrame frame, Account acc) {
+		setLayout(null);
+		
+		JLabel lblWelcome = new JLabel("Welcome to the UofC\n");
+		lblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWelcome.setFont(new Font("LM Mono 10", Font.BOLD, 16));
+		lblWelcome.setBounds(133, 122, 184, 16);
+		add(lblWelcome);
+		
+		JLabel lblName = new JLabel("Hello, <dynamic>!");
+		lblName.setFont(new Font("LM Mono 10", Font.BOLD, 14));
+		lblName.setHorizontalAlignment(SwingConstants.CENTER);
+		lblName.setBounds(138, 170, 173, 16);
+		add(lblName);
+		
+		JLabel lblUofcJournalSubmission = new JLabel("Journal Submission System!");
+		lblUofcJournalSubmission.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUofcJournalSubmission.setFont(new Font("LM Mono 10", Font.BOLD, 16));
+		lblUofcJournalSubmission.setBounds(97, 144, 256, 16);
+		add(lblUofcJournalSubmission);
+	}
+}
