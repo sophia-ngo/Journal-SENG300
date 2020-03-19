79e137729237dbb9b806b14fc1f1c7d35579b5e8
mohsinraza02 <47283171+mohsinraza02@users.noreply.github.com>
2020-03-19 16:14:50 -0600

    Add files via upload

--- EditorGUI.java
+++ EditorGUI.java
@@ -6,6 +6,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -20,49 +21,53 @@ public class EditorGUI extends JPanel {
 	/**
 	 * Create the panel.
 	 */
-	public EditorGUI() {
+	public EditorGUI(JFrame frame, Authenticator auth) {
+		setLayout(null);
+		
 		String[] Reviewers= (EditorChooseReviewer.reviewers.clone());
 		
-		JLabel SelectReviewer = new JLabel("Select Reviewer");
-		add(SelectReviewer);
-		JComboBox Reviewerlist = new JComboBox(Reviewers);
-		Reviewerlist.setEditable(true);
-		Reviewerlist.setMaximumRowCount(100);
-		add(Reviewerlist);
 		
-		JButton btnNewButton = new JButton("Assign");
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				System.out.println("Reviewer Assigned");
-				
-			}
-		});
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
 		
-		JLabel lblNewLabel_1 = new JLabel("Select Reviewer 2");
-		add(lblNewLabel_1);
+		JComboBox comboBox = new JComboBox(Reviewers);
+		comboBox.setBounds(159, 16, 36, 26);
+		add(comboBox);
+		
+		JLabel Reviewer1 = new JLabel("Select Reviewer 1");
+		Reviewer1.setBounds(15, 19, 141, 20);
+		add(Reviewer1);
 		
 		JComboBox comboBox_1 = new JComboBox(Reviewers);
+		comboBox_1.setBounds(159, 80, 36, 26);
 		add(comboBox_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("Select Reviewer 3");
-		add(lblNewLabel_2);
+		JLabel Reviewer2 = new JLabel("Select Reviewer 2");
+		Reviewer2.setBounds(15, 83, 129, 20);
+		add(Reviewer2);
 		
 		JComboBox comboBox_2 = new JComboBox(Reviewers);
+		comboBox_2.setBounds(159, 146, 36, 26);
 		add(comboBox_2);
 		
-		JLabel lblNewLabel = new JLabel("Select Paper");
-		add(lblNewLabel);
+		JLabel reviewer3 = new JLabel("Select Reviewer 3");
+		reviewer3.setBounds(15, 149, 129, 20);
+		add(reviewer3);
+		
+		JComboBox comboBox_3 = new JComboBox();
+		comboBox_3.setBounds(361, 16, 36, 26);
+		add(comboBox_3);
 		
 		String[] Submissions = { };
-		JComboBox comboBox = new JComboBox();
-		add(comboBox);
-		add(btnNewButton);
+		JLabel lblNewLabel = new JLabel("Submission");
+		lblNewLabel.setBounds(242, 19, 104, 20);
+		add(lblNewLabel);
+		
+		JButton editorAssign = new JButton("Assign");
+		editorAssign.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		editorAssign.setBounds(282, 145, 115, 29);
+		add(editorAssign);
 
 	}
-
 }
