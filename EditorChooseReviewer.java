2bbe612a856c71b961c60ddd53921418c371cfaf
mohsinraza02 <47283171+mohsinraza02@users.noreply.github.com>
2020-03-19 14:46:15 -0600

    Add files via upload

--- EditorChooseReviewer.java
+++ EditorChooseReviewer.java
@@ -1,8 +1,17 @@
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
 
 public class EditorChooseReviewer {
-	public static String reviewers[] = { };
 	
+
+
+		public static String reviewers[] = { };
+		
+	/*
 	public static void main(String[] args)
 	{
 		ReadFile rf = new ReadFile();
@@ -21,4 +30,19 @@ public class EditorChooseReviewer {
 		}
 	}
 }
+*/
+		public static void main(String[] args) throws Exception {
+			List<String> reviewers = Collections.emptyList(); 
+			try 
+			{ 
+				reviewers = Files.readAllLines(Paths.get("accounts.txt"), StandardCharsets.UTF_8); 
+			} catch 
+				(IOException e) { 
+				// TODO Auto-generated catch block 
+				e.printStackTrace(); 
+				} 
+			System.out.println("Content of List:"); 
+			System.out.println(reviewers);
+		}
+	}
 
